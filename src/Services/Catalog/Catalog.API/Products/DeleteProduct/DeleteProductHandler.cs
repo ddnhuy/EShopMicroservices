3ec0ae148d14848fa28bf2f10257144f596018e6
ddnhuy <doan.nhathuy.03@gmail.com>
2025-04-15 11:36:35 +0700

    Add LoggingBehavior

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -13,14 +13,11 @@
     }
 
     internal class DeleteProductCommandHandler(
-        IDocumentSession session,
-        ILogger<DeleteProductCommandHandler> logger)
+        IDocumentSession session)
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
-
             var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
 
             if (product is null)
